6ef2025b2cec3193a113b1742d14c00cf34af4d3
yogatresnaa <yogatresnaa@gmail.com>
2023-10-19 15:32:52 +0700

    add page not found

---
+++ src/pages/NotFound/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NotFound from './component/notFound';
+
+function PageNotFound() {
+  return (
+    <div>
+      <NotFound />
+    </div>
+  );
+}
+
+export default PageNotFound;
---
+++ src/pages/NotFound/component/notFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div className='not-found'>
+      <h1>404</h1>
+      <h3>Oops! Page Not Found </h3>
+    </div>
+  );
+}
+
+export default NotFound;
