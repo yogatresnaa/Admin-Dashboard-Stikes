4fcd3622a59bd0144ff6e4631b16b317e354fc4b
dwikysahut <dwikysahut@gmail.com>
2024-06-09 14:17:59 +0700

    fix auth me

--- src/utils/http.js
+++ src/utils/http.js
@@ -38,8 +38,8 @@ export const loginUser = (body) => Axios.post(`${URL_BASE}/auth/login`, body)
 export const logoutUser = (body) => Axios.post(`${URL_BASE}/auth/logout`, body)
 export const registerUser = (body) =>
     Axios.post(`${URL_BASE}/auth/register`, body)
-export const checkMe = (token) =>
-    Axios.post(`${URL_BASE}/auth/me`, token, options(token))
+export const checkMe = (value) =>
+    Axios.post(`${URL_BASE}/auth/me`, value, options(value?.token))
 
 export const getAllProdi = (query = {}, token) =>
     Axios.get(`${URL_BASE}/program-studi`, { ...options(token), params: query })
