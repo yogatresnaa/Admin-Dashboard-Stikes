44dbfb0768abef98ee1376d9d7174b6762294de2
Josua <josuawilliams17@gmail.com>
2024-07-07 15:35:21 +0700

    add env

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,7 +2,7 @@ import Axios from 'axios'
 import { logoutUserAction } from '../redux/actions/actionsTypes'
 import { logoutUserActionCreator } from '../redux/actions/authAction'
 
-const URL_BASE = 'http://localhost:5000'
+const URL_BASE = import.meta.env.REACT_APP_BASE_URL
 const options = (token) => ({
     headers: { Authorization: `Bearer ${token}` },
     timeout: 15000,
